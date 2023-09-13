b424b2a799be84b53c4fb91f62f71a0372092d47
faiziicheema <128216409+faiziicheema@users.noreply.github.com>
2023-09-13 17:13:48 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,14 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
-mongoose.connect(process.env.MONGO_ATLAS_URL);
-
+mongoose.connect(
+  "mongodb+srv://faizyjutt11:Faiziii9611@cluster0.xsnxxoo.mongodb.net/Skill_Nest_Institue?retryWrites=true&w=majority"
+);
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
 
 const courseRouter = require("./routes/courses");
 const userRouter = require("./routes/user");
@@ -24,7 +26,9 @@ const categoriesRouter = require("./routes/categoreis");
 app.use("/courses", courseRouter);
 app.use("/user", userRouter);
 app.use("/categories", categoriesRouter);
-
+app.get("/", (req, res) => {
+  res.json("Hello Backend");
+});
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port 4000`);
 });
